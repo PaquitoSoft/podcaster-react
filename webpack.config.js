7bda1a3daf678ed862274244cb89dfe8e840568e
PaquitoSoft <paquitosoftware@gmail.com>
2015-08-16 01:59:11 +0200

    Styles imported using web pack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,14 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel'
-			}
+			},
+			// https://github.com/gowravshekar/bootstrap-webpack
+			{ test: /\.css$/, loader: 'style-loader!css-loader' },
+			{ test: /\.woff$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+			{ test: /\.woff2$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff2' },
+			{ test: /\.ttf$/, loader: 'file-loader' },
+			{ test: /\.eot$/, loader: 'file-loader' },
+			{ test: /\.svg$/, loader: 'file-loader' }
 		]
 	}
 };
